a3b7d9bebc5b1348cd6498c75059d4e1e05931b0
stupchen <81515728+stupchen@users.noreply.github.com>
2024-08-24 12:40:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -105,6 +105,10 @@ def get_roomid(args1, args2):
 
 
 if __name__ == "__main__":
+    currentTime = time.strftime("%H:%M:%S", time.localtime(time.time()))
+    while (currentTime != "11:18:00"):
+      currentTime = time.strftime("%H:%M:%S", time.localtime(time.time()))
+      print(currentTime)
     config_path = os.path.join(os.path.dirname(__file__), 'config.json')
     parser = argparse.ArgumentParser(prog='Chao Xing seat auto reserve')
     parser.add_argument('-u','--user', default=config_path, help='user config file')
